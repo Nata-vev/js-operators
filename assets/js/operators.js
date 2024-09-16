c10fc73562c3e8baa547a8ce77d3d0d3395cbaf4
Nata-vev <gnataly2589@gmail.com>
2024-09-16 16:53:00 +0300

    Add operators

---
+++ assets/js/operators.js
@@ -0,0 +1,20 @@
+
+let number1 = 1;
+let number2 = 2;
+let resultTask1 = number1 + number2;
+console.log(resultTask1);
+
+
+let resultTask2 = 50 - resultTask1;
+console.log(resultTask2);
+
+let resultTask3 = resultTask1 * resultTask2;
+console.log(resultTask3);
+
+let resultTask4 = resultTask3 / 2.687;
+console.log(resultTask4);
+let remainder = resultTask4 % 3;
+console.log(remainder);
+
+let stringResultTask4 = '' + resultTask4;
+console.log(stringResultTask4);
