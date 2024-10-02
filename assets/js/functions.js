cb58706527608a41ad210ec0ec4cec51734821ae
Nata-vev <gnataly2589@gmail.com>
2024-10-02 18:23:04 +0300

    Fix functions and operators

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,62 +1,66 @@
+// 1. Створити функцію, яка буде знаходити та повертати найменьше з двох чисел. Зробити на початку перевірки, які мають стежити за тим щоб у функцію не передали некорректні дані при її запуску. У випадку передачі хоч одного некорректного параметру фунція має одразу повернути null.
 
-// const getMinNumber = function (number1, number2) {
-//   if (number1 > number2) {
-//     return number2;
-//   } else {
-//     return number1;
-//   }
-// };
-// console.log(getMinNumber(4, 5));
+const getMinNumber = function (number1, number2) {
+  if (typeof number1 !== "number" ||
+    typeof number2 !== "number" ||
+    isNan(number1 - number2)) {
+    return null;
+  }
+  if (number1 > number2) {
+    return number2;
+  } else {
+    return number1;
+  }
+};
+console.log(getMinNumber(4, 5));
 
-const  getMinNumber = function (number1, number2){
-  return number1 > number2 ? number2 : number1; 
-}
-console.log(getMinNumber(15, 7));
+// тернарка
+// const  getMinNumber = function (number1, number2){
+//   return number1 > number2 ? number2 : number1; 
+// }
+// console.log(getMinNumber(15, 7));
 
-
-// const isAdult = function (age) {
-//   if (age >= 18) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-// console.log(isAdult(17));
+//2. Створити функцію, яка буде отимувати вік людини і казати, повнолітня вона, чи ні. Зробити на початку перевірки, які мають стежити за тим щоб у функцію не передали некорректні дані при її запуску. У випадку передачі хоч одного некорректного параметру фунція має одразу повернути null.
 
 const isAdult = function (age) {
-  return age >= 18 ? true : false;
+  if (typeof age !== 'number' || isNan(age)) {
+    return null;
+  }
+  return age >= 18;
 }
 console.log(isAdult(35));
 
+// 3. Створити функцію, яка буде приймати від користувача загальну суму його покупки та буде повертати суму з урахуванням знижки. Базова знижка у 5% дається усім покупкам, але при загальній вартості покупки від 1500грн знижка має складати 10%.
+// Зробити на початку перевірки, які мають стежити за тим щоб у функцію не передали некорректні дані при її запуску. У випадку передачі хоч одного некорректного параметру фунція має одразу повернути null.
 
-// const getDiscount = function (price) {
-//   if (price >= 1500) {
-//     return price - (price / 100 * 10);
-//   } else {
-//     return price - (price / 100 * 5);
-//   }
-// };
-// console.log(getDiscount(2300));
-
-const getDiscount = function (price) {
-  return price  >= 1500 ? price - price / 100 * 10 :  price - price / 100 * 5;
+const getDiscountPrice = function (price) {
+  if (typeof price !== 'number' || isNan(price)) {
+    return null;
   }
-  console.log(getDiscount(1500));
-  
+  if (price >= 1500) {
+    return price - (price / 100 * 10);
+  } else {
+    return price - (price / 100 * 5);
+  }
+};
+console.log(getDiscountPrice(2300));
 
-// if (isNaN(firstNumber - secondNumber)) {
-//   return null;
-// };
-// const isEven = function (number) {
-//   if (number % 2 === 0) {
-//     return true;
-//   } else {
-//     return false;
+// тернарка
+// const getDiscountPrice = function (price) {
+//   return price  >= 1500 ? price - price / 100 * 10 :  price - price / 100 * 5;
 //   }
-// };
-// console.log(isEven(9));
+//   console.log(getDiscount(1500));
+
+
+
+// Створити функцію яка буде приймати число та казати, чи є воно парним. Парні числа завжди діляться без залишку на 2.
+// Зробити на початку перевірки, які мають стежити за тим щоб у функцію не передали некорректні дані при її запуску. У випадку передачі хоч одного некорректного параметру фунція має одразу повернути null.
 
 const isEven = function (number) {
-  return number % 2 === 0 ? true : false;
-}
-console.log(isEven(11));
+  if (typeof number !== 'number' || isNan(number)) {
+    return null;
+  }
+  return number % 2 === 0;
+};
+console.log(isEven(9));
+
--- assets/js/operators.js
+++ assets/js/operators.js
@@ -1,21 +1,37 @@
 
+// 1. Створити дві числові змінні, зберегти результат їх суми у третю змінну та вивести отримане значення у консоль
 let number1 = 1;
 let number2 = 2;
 let resultTask1 = number1 + number2;
 console.log(resultTask1);
 
-
+// 2. З результуючої змінної першого завдання відняти 50, зберігти результат у новій змінній та вивести у консоль
 let resultTask2 = 50 - resultTask1;
 console.log(resultTask2);
 
+// 3. Змінну з результатом другого завдання помножити на змінну з результатом першого завдання, зберігти результат у новій змінній та вивести у консоль
 let resultTask3 = resultTask1 * resultTask2;
 console.log(resultTask3);
 
+// 4. Змінну з результатом третього завдання поділити на 2.687, результат зберігти у нову змінну та вивести її в консоль
+// Також в окрему змінну винести залишок від ділення на те саме число і вивести у консоль
 let resultTask4 = resultTask3 / 2.687;
 console.log(resultTask4);
 let remainder = resultTask4 % 2.687;
 console.log(remainder);
 
+
+// 5. Перетворити будь-яку змінну з пункту 4 на рядок та зберегти його у якусь іншу змінну
 let stringResultTask4 = '' + resultTask4;
 console.log(stringResultTask4);
+
+
+
+
+
+
+
+
+
+
 
